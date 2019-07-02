5a924748c33de974df6b6975358f8d03e0582fb5
Brad Williams <merlosus@yahoo.com>
2019-07-02 18:34:21 +1000

    renamed to app.py for openshift
    Signed-off-by: Brad Williams <merlosus@yahoo.com>

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ if __name__ == '__main__':
     model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
     print ('Model columns loaded')
 
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=8080)
