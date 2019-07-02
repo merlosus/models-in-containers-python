f493714b4e4b0ef9c1637c2c830d10bc2ba53c97
Brad Williams <merlosus@yahoo.com>
2019-07-02 18:26:55 +1000

    renamed to app.py for openshift
    Signed-off-by: Brad Williams <merlosus@yahoo.com>

--- app.py
+++ app.py
@@ -31,14 +31,10 @@ def predict():
 
 
 if __name__ == '__main__':
-    try:
-        port = int(sys.argv[1]) # This is for a command-line input
-    except:
-        port = 12345 # If you don't provide any port the port will be set to 12345
 
     lr = joblib.load("model.pkl") # Load "model.pkl"
     print ('Model loaded')
     model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
     print ('Model columns loaded')
 
-    app.run(port=port, debug=True)
+    app.run(host='0.0.0.0', port=8080)
