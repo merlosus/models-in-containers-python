5502eab60002b0b76f9f2bfef8d7215ab1538f83
Brad Williams <merlosus@yahoo.com>
2019-07-02 19:06:01 +1000

    testing config file
    Signed-off-by: Brad Williams <merlosus@yahoo.com>

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
-#!/usr/bin/python
+# Dependencies
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import traceback
+import pandas as pd
+import numpy as np
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+if 'OPENSHIFT_APP_NAME' in os.environ:              #are we on OPENSHIFT?
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+else:
+    ip = '0.0.0.0'                            #localhost
+    port = 8051
 
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
+# Your API definition
+app = Flask(__name__)
 
-# Importing Application
 
-from api import app as application
+@app.route('/predict', methods=['POST'])
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first.')
+        return ('No model here to use')
+
+
+if __name__ == '__main__':
+
+    lr = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
+    app.run(port=port,host=ip)
