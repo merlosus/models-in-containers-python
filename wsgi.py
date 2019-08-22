f64289d51cd5a3be20c8c38ff99eca10b1594715
Brad Williams <merlosus@yahoo.com>
2019-08-22 20:47:51 +1000

    Added support for async parallel processing.

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 # Dependencies
 from flask import Flask, request, jsonify
-from sklearn.externals import joblib
+from joblib import Parallel, delayed
 import traceback
 import pandas as pd
+import joblib
 import numpy as np
 
 # API definition
@@ -18,14 +19,22 @@ def index():
 def predict():
     if lr:
         try:
+            prep = []
             json_ = request.json
             print(json_)
             query = pd.get_dummies(pd.DataFrame(json_))
             query = query.reindex(columns=model_columns, fill_value=0)
+            n_chunks = 5
+            n_samples = query.shape[0]
+            slices = np.array_split(query, n_chunks)
+            slices = [query for query in slices if query.size > 0]
+            jobs = (delayed(lr.predict)(array) for array in slices)
+            parallel = Parallel(n_jobs=n_chunks)
+            results = parallel(jobs)
+            for item in results:
+                prep.append(item.tolist())
 
-            prediction = list(lr.predict(query))
-
-            return jsonify({'prediction': str(prediction)})
+            return jsonify({'prediction': str(list(prep[0]))})
 
         except:
 
